0ff2133ca77278110d46fc8950e41916174485d9
milosh-96 <milosjovanovic042@gmail.com>
2018-07-18 22:05:01 +0200

    model question

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['title','slug','description','ip_address','enable_discussion','enable_answers'];
 }
