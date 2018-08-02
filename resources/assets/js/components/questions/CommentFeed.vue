fa807e5400b66ca94275f4233d90a6ea9d09f259
Miloš <milos@Markos-MacBook.local>
2018-08-02 05:19:16 +0200

    vue comment-feed

--- resources/assets/js/components/questions/CommentFeed.vue
+++ resources/assets/js/components/questions/CommentFeed.vue
@@ -1,15 +1,28 @@
 <template>
-     <ul class="list-group">
-        <li class="list-group-item border-0">
-            <a href="">User1</a>: YOO HOO
-        </li>
-    </ul>
+     <div class="comment-feed">
+        <div v-if="loading"><em>Comments are being loaded...</em></div>
+        <div v-else><
+        <ul class="list-group">
+            <li class="list-group-item border-0">
+                <a href="">User1</a>: YOO HOO
+            </li>
+         </ul>     
+        </div>
+     </div>
 </template>
 
 <script>
     export default {
         mounted() {
             console.log('Component mounted.')
+        },
+        data() {
+            return {
+                loading:true
+            }
+        },
+        methods: {
+
         }
     }
 </script>
