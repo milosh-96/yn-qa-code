744cd22062093bcfb7db7d6f1fb850e8681183cd
Miloš <milos@Markos-MacBook.local>
2018-08-13 03:23:55 +0200

    comment dropdown: report option available to public (non logged users)

--- resources/views/questions/partials/comment.blade.php
+++ resources/views/questions/partials/comment.blade.php
@@ -6,19 +6,21 @@
         <span @if($comment->isLoggedUserAuthor()) class="comment-text" data-type="text" data-url="{{route('api.comment.update')}}" data-pk="{{$comment->id}}" data-name="comment_text" value="{{$comment->comment_text}}"@endif>{{$comment->comment_text}}</span>
         </div>
         <div class="col-2 text-right">
-        @if($comment->isLoggedUserAuthor())
         <div class="dropdown">
   <a id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     ...
   </a>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+  @if($comment->isLoggedUserAuthor())
+
     <a class="dropdown-item edit-comment" href="#comment-{{$comment->id}}"><i class="fa fa-pencil-alt"></i> Edit</a>
     <a class="dropdown-item delete-comment" href="#comment-{{$comment->id}}"><i class="fa fa-trash"></i> Delete</a>
     <div class="dropdown-divider"></div>
+    @endif  
     <a class="dropdown-item" href="#"><i class="fa fa-exclamation-triangle"></i> Report</a>
   </div>
+
 </div>
-@endif  
     </div>
     <div>
         <small>{{$comment->createdAt()}}</small>
